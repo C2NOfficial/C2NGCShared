19a94f2fcf8a87e503f7d8c4dd2be988741ed5ef
harshmohansason <harshsason2000@gmail.com>
2026-03-20 23:51:08 +0530

    Added refund_initiated status since refunds can be done from admin panel now

--- constants/order.go
+++ constants/order.go
@@ -1,14 +1,15 @@
 package constants
 
 const (
-	ORDER_STATUS_PENDING       = "pending"
-	ORDER_STATUS_PAID          = "paid"
-	ORDER_STATUS_FAILED        = "failed"
-	ORDER_STATUS_SHIPPED       = "shipped"
-	ORDER_STATUS_DELIVERED     = "delivered"
-	ORDER_STATUS_CANCELLED     = "cancelled"
-	ORDER_STATUS_REFUNDED      = "refunded"
-	ORDER_STATUS_RTO_DELIVERED = "rto_delivered"
+	ORDER_STATUS_PENDING          = "pending"
+	ORDER_STATUS_PAID             = "paid"
+	ORDER_STATUS_FAILED           = "failed"
+	ORDER_STATUS_SHIPPED          = "shipped"
+	ORDER_STATUS_DELIVERED        = "delivered"
+	ORDER_STATUS_CANCELLED        = "cancelled"
+	ORDER_STATUS_REFUND_INITIATED = "refund_initiated"
+	ORDER_STATUS_REFUNDED         = "refunded"
+	ORDER_STATUS_RTO_DELIVERED    = "rto_delivered"
 )
 
 var ORDER_STATUSES_MAP = map[string]bool{
