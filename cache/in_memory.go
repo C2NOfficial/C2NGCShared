3e4c8823e04a6d2acffd0be4b97b22b87d931058
harshmohansason <harshsason2000@gmail.com>
2026-03-20 20:05:48 +0530

    Removed time from the cache type structs since google cloud function auto restarts within 5-10m of activity. No point of adding unecessary complexity right now. If traffic increases in future, will move to redis

--- cache/in_memory.go
+++ cache/in_memory.go
@@ -2,27 +2,25 @@ package cache
 
 import (
 	"sync"
-	"time"
 )
 
-var ShippingEstimateCache = NewCache[float64](30 * time.Minute)
-var IPCache = NewCache[int](30 * time.Minute)
+// Stores the cost of shipment for an order
+var ShippingEstimateCache = NewCache[float64] 
+// Stores the count for each ip how many times it requested
+var IPCache = NewCache[int] 
 
 type entry[T any] struct {
-	Value     T
-	ExpiresAt time.Time
+	Value T
 }
 
 type Cache[T any] struct {
 	entries map[string]entry[T]
-	ttl     time.Duration
-	lock    sync.RWMutex //map is not thread safe in go.
+	lock    sync.RWMutex //Map is not thread safe
 }
 
-func NewCache[T any](ttl time.Duration) *Cache[T] {
+func NewCache[T any]() *Cache[T] {
 	return &Cache[T]{
 		entries: make(map[string]entry[T]),
-		ttl:     ttl,
 	}
 }
 
@@ -30,18 +28,11 @@ func (c *Cache[T]) Get(key string) (T, bool) {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 	e, ok := c.entries[key]
-	if !ok || time.Now().UTC().After(e.ExpiresAt) {
-		var zero T
-		return zero, false
-	}
-	return e.Value, true
+	return e.Value, ok
 }
 
 func (c *Cache[T]) Set(key string, value T) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
-	c.entries[key] = entry[T]{
-		Value:     value,
-		ExpiresAt: time.Now().UTC().Add(c.ttl),
-	}
+	c.entries[key] = entry[T]{Value: value}
 }
