0ccbcb46d29d5457830766f285a03d8ff246f786
harshmohansason <harshsason2000@gmail.com>
2026-03-20 23:52:55 +0530

    Added generate hash for refund initiate hash and the salt for the generate hash functions does not need to be passed via parameters since salt is initialized in the package init function itself

--- payu/hash.go
+++ payu/hash.go
@@ -8,20 +8,20 @@ import (
 )
 
 // Reference: https://docs.payu.in/docs/generate-hash-payu-hosted
-func GenerateInitialHash(p *PaymentRequest, salt string) string {
+func GenerateInitialHash(p *PaymentRequest) string {
 	hashString := fmt.Sprintf(
 		"%s|%s|%s|%s|%s|%s|||||||||||%s",
-		p.Key, p.TxnID, p.Amount, p.ProductInfo, p.Firstname, p.Email, salt,
+		p.Key, p.TxnID, p.Amount, p.ProductInfo, p.Firstname, p.Email, Salt,
 	)
 	hash := sha512.Sum512([]byte(hashString))
 	return hex.EncodeToString(hash[:])
 }
 
 // Reference: https://docs.payu.in/docs/generate-hash-payu-hosted
-func GenerateResponseHash(form url.Values, salt string) string {
+func GenerateResponseHash(form url.Values) string {
 	hashString := fmt.Sprintf(
 		"%s|%s||||||%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s",
-		salt,
+		Salt,
 		form.Get("status"),
 		form.Get("udf5"),
 		form.Get("udf4"),
@@ -35,6 +35,13 @@ func GenerateResponseHash(form url.Values, salt string) string {
 		form.Get("txnid"),
 		form.Get("key"),
 	)
+	hash := sha512.Sum512([]byte(hashString))
+	return hex.EncodeToString(hash[:])
+}
+
+// Reference: // Reference: https://docs.payu.in/reference/refund_transaction_api
+func GenerateRefundInitateHash(rir *RefundInitiationRequest) string {
+	hashString := fmt.Sprintf("%s|%s|%s|%s", rir.Key, rir.Command, rir.Var1, Salt)
 	hash := sha512.Sum512([]byte(hashString))
 	return hex.EncodeToString(hash[:])
 }
