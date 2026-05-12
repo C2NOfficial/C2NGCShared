aa9db80088dcc480e70bfcff06672ad0b0ac8dbe
harshmohansason <harshsason2000@gmail.com>
2026-05-13 01:42:09 +0530

    Added function add to contact list. Currently adding only to list id 6 for website newsletters

---
+++ brevo/contacts.go
@@ -0,0 +1,51 @@
+package brevo
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	baseURL = "https://api.brevo.com/v3/contacts"
+)
+
+func AddToContactList(email string) error {
+	payload := AddContactPayload{
+		Email:         email,
+		ListIds:       []int{6}, 
+		UpdateEnabled: true,
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, baseURL, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("api-key", API_KEY)
+	req.Header.Set("content-type", "application/json")
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("brevo API error: status %s, body: %s", resp.Status, string(body))
+	}
+
+	return nil
+}
